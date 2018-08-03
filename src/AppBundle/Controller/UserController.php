41d163430e2f982cffd26222e9fdb9d64214cc0a
salvador.perez <salvador.perez@innoforma.com>
2018-08-03 10:06:01 +0200

    -- creanco usuario admin e inicio de sesión

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use AppBundle\Utils\Filter;
+
+
+class UserController extends Controller
+{
+
+    private $request, $validator, $em;
+
+    function __construct()
+    {
+        $this->request = Request::createFromGlobals();
+        $this->validator = $this->get('validator');
+        $this->em = $this->getDoctrine();
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('login/login.html.twig');
+    }
+
+    /**
+     * @Route("/register-admin")
+     */
+    //Provisional , solo se ha insertado un admin pasando parámetros a mano
+    public function registerAdmin()
+    {
+
+        $user = new User();
+        $user->read();
+        //codificar password
+        $user->setPassword($user->getPassword());
+
+        $this->em->getManager()->persist($user);
+        $this->em->getManager->flush();
+
+
+        return $this->render('pruebas/prueba.html.twig', ['user' => $user]);
+
+    }
+
+    /**
+     * @Route("/login")
+     */
+    public function login(){
+        $user = new User();
+
+        $login = $this->request->query->get('nick');
+        $password = $this->request->query->get('password');
+
+        //Comprobar si se loguea con correo o con nick
+        if(Filter::isEmail($login)){
+            //obtener usuario por email
+        }else{
+            //obtener usuario por nick
+        }
+        //Comprobar que la clave sea la correcta
+
+
+    }
+}
---
+++ src/AppBundle/Utils/Session.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salvador.perez
+ * Date: 03/08/2018
+ * Time: 9:45
+ */
+
+namespace AppBundle\Utils;
+
+
+class Session
+{
+    function __construct($name = null) {
+        if (session_status() === PHP_SESSION_NONE) {
+            if ($name !== null) {
+                session_name($name);
+            }
+            session_start();
+        }
+    }
+
+    function close() {
+        $this->delete('__user');
+    }
+
+    function delete($name) {
+        if (isset($_SESSION[$name])) {
+            unset($_SESSION[$name]);
+        }
+        return $this;
+    }
+
+    function destroy() {
+        session_destroy();
+    }
+
+    function get($name) {
+        $r = null;
+        if (isset($_SESSION[$name])) {
+            $r = $_SESSION[$name];
+        }
+        return $r;
+    }
+
+    function getUser() {
+        return $this->get("__user");
+    }
+
+    function isLogged() {
+        return $this->getUser() !== null;
+    }
+
+    // Bloqueo de sesión
+    function isBlocked($block = null){
+        if ($block == null){
+            return $this->get("__blocked");
+        }else{
+            return $this->set("__blocked", $block);
+        }
+    }
+
+    //start alias
+
+    function login($user) {
+        return $this->setUser($user);
+    }
+
+    function logout() {
+        $this->close();
+    }
+
+    //end alias
+
+    function set($name, $value) {
+        $_SESSION[$name] = $value;
+        return $this;
+    }
+
+    function setUser($user) {
+        session_regenerate_id();
+        return $this->set("__user", $user);
+    }
+}
